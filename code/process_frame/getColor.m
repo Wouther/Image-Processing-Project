1cb8a36d73301822f5f19d17f7bf01209804109c
Wouther <wouther.bons@gmail.com>
2016-01-06 15:54:52 +0100

    Character recognition files moved
    Moved to process_frame folder.

---
+++ code/process_frame/getColor.m
@@ -0,0 +1,7 @@
+function y = getColor(img,color,threshold)
+    a(:,:,1,:)=img(:,:,1,:)-color(1);
+    a(:,:,2,:)=img(:,:,2,:)-color(2);
+    a(:,:,3,:)=img(:,:,3,:)-color(3);
+    b=sum(abs(a),3);
+    y=b<(threshold);
+end
--- code/process_frame/process_frame.m
+++ code/process_frame/process_frame.m
@@ -8,7 +8,8 @@ function [license_plate, processed_img] = process_frame(img)
     %Find license plate in image
     %img2 = nummerbordvinder(img)
     
-    
+    %Perform character recognition on license plate image
+    %license_plate = ??? %TODO: how? Files added: getColor.m, getLetter.m and lettermap.m
     
     processed_img = img; %temporarily as test
 end
