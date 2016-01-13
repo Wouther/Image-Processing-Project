b836cd6034d45802a2afa9d26d4ba2c5905fb105
Wouther <wouther.bons@gmail.com>
2016-01-13 12:17:08 +0100

    Indentation

--- code/process_frame/letterMapWrapper.m
+++ code/process_frame/letterMapWrapper.m
@@ -1,50 +1,50 @@
 function l= letterMapWrapper(images,letters,numbers,minussign,forms,xormax)
-siz=size(images);
-siz=siz(2);
-siz2=size(forms);
-siz2=siz2(1);
-tmp=zeros(3,siz);
-errors=zeros(3,siz2);
-sums=zeros(1,siz2);
-if(siz~=8)
-    l='e';
-    return;
-end
-for(n=1:siz)
-    [tmp,tmp2]=lettermap(images(n).image,letters,xormax);
-    errors(1,n)=tmp2;
-    chars(1,n)=tmp;
-end;
-for(n=1:siz)
-    [tmp,tmp2]=lettermap(images(n).image,numbers,xormax);
-    errors(2,n)=tmp2;
-    chars(2,n)=tmp;
-end;
-for(n=1:siz)
-    [tmp,tmp2]=lettermap(images(n).image,minussign,xormax);
-    errors(3,n)=tmp2;
-    chars(3,n)=tmp;
-end;
-for(n=1:siz2)
-    tmp(1)=0;
-    for(n2=1:siz)
-        tmp(n2)=errors(forms(n,n2),n2);
+    siz=size(images);
+    siz=siz(2);
+    siz2=size(forms);
+    siz2=siz2(1);
+    tmp=zeros(3,siz);
+    errors=zeros(3,siz2);
+    sums=zeros(1,siz2);
+    if(siz~=8)
+        l='e';
+        return;
+    end
+    for(n=1:siz)
+        [tmp,tmp2]=lettermap(images(n).image,letters,xormax);
+        errors(1,n)=tmp2;
+        chars(1,n)=tmp;
     end;
-    sums(n)=sum(tmp);
-end;
-max=1000000;
-result=0;
-for(n=1:siz2)
-    sums(n)
-    n
-    if(sums(n)<max)
-        max=sums(n);
-        result=n;
+    for(n=1:siz)
+        [tmp,tmp2]=lettermap(images(n).image,numbers,xormax);
+        errors(2,n)=tmp2;
+        chars(2,n)=tmp;
     end;
-end;
-form=forms(result,:);
-for(n=1:siz)
-    l(n)=chars(form(n),n);
-end;
+    for(n=1:siz)
+        [tmp,tmp2]=lettermap(images(n).image,minussign,xormax);
+        errors(3,n)=tmp2;
+        chars(3,n)=tmp;
+    end;
+    for(n=1:siz2)
+        tmp(1)=0;
+        for(n2=1:siz)
+            tmp(n2)=errors(forms(n,n2),n2);
+        end;
+        sums(n)=sum(tmp);
+    end;
+    max=1000000;
+    result=0;
+    for(n=1:siz2)
+        sums(n)
+        n
+        if(sums(n)<max)
+            max=sums(n);
+            result=n;
+        end;
+    end;
+    form=forms(result,:);
+    for(n=1:siz)
+        l(n)=chars(form(n),n);
+    end;
 
 end
