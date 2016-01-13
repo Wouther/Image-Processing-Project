22f4dc5704e20fefb717b2d2dbe5c0994bfcdbb5
8uurg <arthur.guijt12@gmail.com>
2016-01-13 12:12:42 +0100

    Improve binarify.

--- code/process_frame/numbinarify.m
+++ code/process_frame/numbinarify.m
@@ -4,14 +4,24 @@ function [ bin ] = numbinarify( image )
     %%
     proc = dip_image(rgb2gray( image ));
     %%
+    % Remove shadow
+    afol = maxf(proc,11,'elliptic');
+    proc = proc + (255 - afol);
+    %%
     % Sharpen to improve results for smaller signs. Could be made optional
     % for larger images.
     proc = proc - 2 * laplace(proc);
+    
+    %% 
+    % Some extra filtering for border pixels
+    proc = maxf(proc, 2, 'elliptic');
+    proc = minf(proc, 2, 'elliptic');
     %%
     proc = ~threshold(proc, 'isodata', Inf);
     
-    %%
-    proc = opening(proc, 2);
+    %% Remove tiny noise 1
+    proc =  bopening(proc, 1);
+    
     %%
     bin = proc;
 end
