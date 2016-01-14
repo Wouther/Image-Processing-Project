e3c7a4e8d38c83fa3dce17e2d5d010d786be40b4
8uurg <arthur.guijt12@gmail.com>
2016-01-14 11:14:44 +0100

    Improve binarification

--- code/process_frame/numbinarify.m
+++ code/process_frame/numbinarify.m
@@ -2,7 +2,7 @@ function [ bin ] = numbinarify( image )
 %NUMBINARIFY Turns a given lp into a binary image.
 %   Detailed explanation goes here
     %%
-    proc = dip_image(rgb2gray( image ));
+    proc = dip_image(image(:, :, 1));
     %%
     % Remove shadow
     afol = maxf(proc,11,'elliptic');
@@ -10,18 +10,19 @@ function [ bin ] = numbinarify( image )
     %%
     % Sharpen to improve results for smaller signs. Could be made optional
     % for larger images.
-    proc = proc - 2 * laplace(proc);
-    
-    %% 
-    % Some extra filtering for border pixels
-    proc = maxf(proc, 2, 'elliptic');
-    proc = minf(proc, 2, 'elliptic');
+    proc = proc - 4 * laplace(proc);
+
     %%
     proc = ~threshold(proc, 'isodata', Inf);
     
     %% Remove tiny noise 1
-    proc =  bopening(proc, 1);
-    
+    procx =  bopening(proc, 1, 2, 0);
+    proc = bpropagation(procx, proc, Inf, -1, 0);
+    %%
+    proc = brmedgeobjs(proc);
+    %% Remove tiny noise 2.
+    proc = label(proc, 2, 15); % Minimal size of any object is 15
+    proc = proc > 0;
     %%
     bin = proc;
 end
